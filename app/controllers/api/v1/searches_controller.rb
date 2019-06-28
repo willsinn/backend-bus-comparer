0d055b81977b0ba4d9a9b25697b804249d773688
William Sinn <w.h.sinn@gmail.com>
2019-06-28 11:16:31 -0400

    add searches_controller

---
+++ app/controllers/api/v1/searches_controller.rb
@@ -0,0 +1,16 @@
+class Api::V1::SearchesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def items
+  end
+    private
+    def search_params
+    end
+end

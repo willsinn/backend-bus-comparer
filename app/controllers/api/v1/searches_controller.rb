23f7a4cd371f6ea11dea25f25555293f766594ca
William Sinn <w.h.sinn@gmail.com>
2019-06-28 11:45:11 -0400

    add searches_controller methods

--- app/controllers/api/v1/searches_controller.rb
+++ app/controllers/api/v1/searches_controller.rb
@@ -1,16 +1,27 @@
 class Api::V1::SearchesController < ApplicationController
+
   def index
+    searches = Search.all
+    render json: searches
   end
 
   def show
+    search = Search.find(params[:id])
+    render json: search
   end
 
   def create
+    create_search = Search.create(search_params)
   end
 
   def items
+    search = Search.find(params[:id])
+    render json: search.items
   end
+
     private
+
     def search_params
+      params.permit(:url, :target_name)
     end
 end
